ed3526fa05ed36faa3758c76c894176c92195380
瞿琛 <931886861@qq.com>
2025-09-26 21:57:24 +0800

    feature:开发短链接后管系统短链接分组绑定用户上下文

--- admin/src/main/java/com/nageoffer/shortlink/admin/controller/GroupController.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/GroupController.java
@@ -3,11 +3,15 @@ package com.nageoffer.shortlink.admin.controller;
 import com.nageoffer.shortlink.admin.common.convention.result.Result;
 import com.nageoffer.shortlink.admin.common.convention.result.Results;
 import com.nageoffer.shortlink.admin.dto.req.ShortLinkGroupSaveReqDTO;
+import com.nageoffer.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
 import com.nageoffer.shortlink.admin.service.GroupService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * 短链接分组控制层
@@ -18,9 +22,24 @@ public class GroupController {
 
     private final GroupService groupService;
 
+    /**
+     * 新增短链接分组
+     * @param requestParam
+     * @return
+     */
     @PostMapping("/api/shortlink/v1/group")
-    private Result<Void> save(@RequestBody ShortLinkGroupSaveReqDTO requestParam) {
+    public Result<Void> save(@RequestBody ShortLinkGroupSaveReqDTO requestParam) {
         groupService.saveGroup(requestParam.getName());
         return Results.success();
+    }
+
+    /**
+     * 查询用户索所有短链接分组
+     * @return
+     */
+    @GetMapping("/api/shortlink/v1/group")
+    public Result<List<ShortLinkGroupRespDTO>> listGroup(){
+
+        return Results.success(groupService.listGroup());
     }
 }
